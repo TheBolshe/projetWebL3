90faa940cebe7763477b134773e04c4e573744b9
TheBolshe <bibsim91@gmail.com>
2019-01-06 17:59:31 +0100

    echoes

--- php/login.php
+++ php/login.php
@@ -3,8 +3,9 @@
   $_SESSION["session_name"] = $_POST["name"];
   $session_folder = "../data/recordings/" . $_SESSION["session_name"];
   if (file_exists($session_folder) && is_dir($session_folder)) {
-
+    echo "Directory exists";
   }else{
     mkdir($session_folder, 0777);
+    echo "Directory created";
   }
 ?>
--- php/write.php
+++ php/write.php
@@ -1,11 +1,8 @@
 <?php
-session_start();
-$path = "../data/recordings/". $_SESSION["session_name"]. "/" . $_POST["name"] . ".json";
-//echo gettype($data), "\n";
-$file = fopen($path, "w") or die("Unable to open file!");
-fwrite($file, $_POST["data"]);
-
-
-fclose($file);
-
+  session_start();
+  $path = "../data/recordings/". $_SESSION["session_name"]. "/" . $_POST["name"] . ".json";
+  $file = fopen($path, "w") or die("Unable to open file!");
+  fwrite($file, $_POST["data"]);
+  echo "Done";
+  fclose($file);
 ?>
